f79bcf16407c30593f99259baa6ad410e58207ed
Lana Virina <lana.virina@gmail.com>
2023-02-25 15:17:49 +0300

    Add function filter_data

--- course_3/utils/funcs.py
+++ course_3/utils/funcs.py
@@ -7,3 +7,15 @@ def load_data(data_path):
     data = raw_data.json()
     return data
 
+
+def data_filter(load_data):
+    """Получает список всех операций, возвращает список операций со статусом EXECUTED"""
+    filter_data = []
+    for dict in load_data:
+        if "state" in dict:
+            if dict["state"] == "EXECUTED":
+                filter_data.append(dict)
+        else:
+            continue
+    return filter_data
+
--- course_3/utils/main.py
+++ course_3/utils/main.py
@@ -1,9 +1,14 @@
 import funcs
+
+
 def main():
     data_path = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb" \
                 "-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp" \
                 "=1677342772554&signature=_C2-rZBAG9ufDhOZg53LDx4-dz-7RXLbUC0CPbGs4IA&downloadName=operations.json"
-    print(funcs.load_data(data_path))
+    data_list = funcs.load_data(data_path)
+    print(data_list)
+    op_executed = funcs.data_filter(data_list)
+    print(op_executed)
 
 
 if __name__ == "__main__":
