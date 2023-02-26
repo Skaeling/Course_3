77d13767e7740a7a3dd6c11ab1cdebc1a3bd2f66
Lana Virina <lana.virina@gmail.com>
2023-02-27 00:48:17 +0300

    Fix docstrings

--- course_3/utils/funcs.py
+++ course_3/utils/funcs.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 
 def load_data(data_path):
-    """Получает json файл операций по ссылке возвращает список"""
+    """Получает json файл операций по ссылке, возвращает список"""
     try:
         response = requests.get(data_path)
         if response.status_code == 200:
@@ -15,27 +15,28 @@ def load_data(data_path):
 
 
 def data_filter(data):
-    """Получает список всех операций, возвращает список операций со статусом EXECUTED"""
+    """Получает список всех операций,
+    возвращает список операций со статусом EXECUTED,
+    объекты, не содержащие статус операции - пропускает"""
     filter_data = []
     for i in data:
-        if "state" in i:
-            if i["state"] == "EXECUTED":
-                filter_data.append(i)
+        if "state" in i and i["state"] == "EXECUTED":
+            filter_data.append(i)
         else:
             continue
     return filter_data
 
 
 def time_filter(data):
-    """Сортирует элементы по ключу "date" от новых к старым,
-    возвращает первые пять значений data"""
+    """Сортирует элементы списка data по значению ключа "date"
+    от новых к старым, возвращает первые пять значений data"""
     data = sorted(data, key=lambda k: k["date"], reverse=True)
     return data[:5]
 
 
 def format_date(data):
-    """Получает дату в формате год-месяц-день-час-минуты-секунды,
-    возвращает дату в формате: день.месяц.год"""
+    """Получает список с датами в формате год-месяц-день-час-минуты-секунды,
+    возвращает список с датами в формате: день.месяц.год"""
     for i in data:
         i["date"] = dt.datetime.strptime(i["date"], '%Y-%m-%dT%H:%M:%S.%f')
         i["date"] = dt.date.strftime(i["date"], '%d.%m.%Y')
