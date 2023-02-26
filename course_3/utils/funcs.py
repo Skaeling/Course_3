1d4ddab65bdf4175e95104769051f5a45c04c2ea
Lana Virina <lana.virina@gmail.com>
2023-02-26 12:48:27 +0300

    Add function time_filter

--- course_3/utils/funcs.py
+++ course_3/utils/funcs.py
@@ -1,21 +1,29 @@
-import requests, datetime
+import requests
+import datetime as dt
 
 
 def load_data(data_path):
-    """Получает json файл операций по ссылке возвращает список словарей"""
-    raw_data = requests.get(data_path)
-    data = raw_data.json()
-    return data
+    """Получает json файл операций по ссылке возвращает список"""
+    data = requests.get(data_path)
+    return data.json()
 
 
-def data_filter(load_data):
+def data_filter(data):
     """Получает список всех операций, возвращает список операций со статусом EXECUTED"""
     filter_data = []
-    for dict in load_data:
-        if "state" in dict:
-            if dict["state"] == "EXECUTED":
-                filter_data.append(dict)
+    for i in data:
+        # сейчас попадают операции без ключа from
+        if "state" in i:
+            if i["state"] == "EXECUTED":
+                filter_data.append(i)
         else:
             continue
     return filter_data
+
+
+def time_filter(data):
+    """Сортирует элементы по ключу "date" от новых к старым,
+    возвращает первые пять значений data"""
+    data = sorted(data, key=lambda k: k["date"], reverse=True)
+    return data[:5]
 
--- course_3/utils/main.py
+++ course_3/utils/main.py
@@ -2,14 +2,15 @@ import funcs
 
 
 def main():
-    data_path = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb" \
-                "-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp" \
-                "=1677342772554&signature=_C2-rZBAG9ufDhOZg53LDx4-dz-7RXLbUC0CPbGs4IA&downloadName=operations.json"
-    data_list = funcs.load_data(data_path)
-    print(data_list)
+    data_url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb" \
+               "-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp" \
+               "=1677490556581&signature=tpg4Nb1QcvIzL-mlMkhnpOo2UjszMNZqM6ewS_OEIQw&downloadName=operations.json"
+    data_list = funcs.load_data(data_url)
     op_executed = funcs.data_filter(data_list)
-    print(op_executed)
+    last_ops = funcs.time_filter(op_executed)
+    print(*last_ops, sep="\n")
 
 
 if __name__ == "__main__":
     main()
+
